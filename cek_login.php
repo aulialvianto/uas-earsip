aeae085bcbf290278cde954a3384221ce0c52235
aulialvianto <pitot.crush@gmail.com>
2021-12-28 11:35:05 +0700

    e

--- cek_login.php
+++ cek_login.php
@@ -1,10 +1,20 @@
 <?php
 session_start();
 include "config/koneksi.php";
-$username=$_POST['username']; 
-$password=$_POST['pass']; 
-$pas=md5($pass);
-$query=mysqli_query($conn,"select * from tbl_user where uname='$username' and pass='$password'");
+
+//contoh login sederhana, bisa dikembang lagi
+
+//password diamankan dengan enkripsi kriptografi md5
+@$pass = md5($_POST['password']);
+
+//mysqli_escape_string fungsinya untuk mengamankan karakter aneh yang diinputkan user, seperti sql injection
+
+@$username = mysqli_escape_string($koneksi, $_POST['username']);
+@$password = mysqli_escape_string($koneksi, $pass);
+
+$login = mysqli_query($koneksi, "SELECT * from tbl_user 
+								 WHERE username='$username' and password = '$password' ");
+$data = mysqli_fetch_array($login);
 if($data)
 {
 	$_SESSION['id_user'] = $data['id_user'];
@@ -18,4 +28,5 @@ else
 			document.location='index.php';
 		  </script>";
 }
- ?> 
+
+?>
